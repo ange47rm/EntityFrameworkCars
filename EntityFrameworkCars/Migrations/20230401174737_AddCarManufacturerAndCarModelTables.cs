144f8441cf4a99fd70b3cb8290fd7649dea9342e
Angelo <Angelo@DESKTOP-22HFJ77>
2023-04-01 19:16:14 +0100

    Add project files.

---
+++ EntityFrameworkCars/Migrations/20230401174737_AddCarManufacturerAndCarModelTables.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkCars.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCarManufacturerAndCarModelTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarManufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearFounded = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarManufacturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateLaunched = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarModels", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarManufacturers");
+
+            migrationBuilder.DropTable(
+                name: "CarModels");
+        }
+    }
+}
---
+++ EntityFrameworkCars/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Xml.Linq;
+
+namespace EntityFrameworkCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class CarManufacturer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int YearFounded { get; set; }
+        }
+
+        public class CarModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTime DateLaunched { get; set; }
+        }
+
+        public class CarsDbContext : DbContext
+        {
+            public DbSet<CarManufacturer> CarManufacturers { get; set; }
+            public DbSet<CarModel> CarModels { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer("Data Source=angelosdb.cd8qygawtti7.eu-west-2.rds.amazonaws.com;Database=postgres;User ID=postgres;Password=angelosdb;MultipleActiveResultSets=True");
+                }
+            }
+        }
+    }
+}
